fd2a603584e4356bf28b7ecc71bba31ce5440eaa
truong_faker <134347167+vantruong1812@users.noreply.github.com>
2023-06-06 10:34:25 +0700

    Update hangso.cpp

--- hangso.cpp
+++ hangso.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define PI 3.1415
 int main() {
-	// dung "const + kieu bien + ten bien"  de tao ra hang so hoac su dung khai bao "#define + ten bien + so"
+	///
 const double  PI = 3.1415; //dung cai nay thi ko dc dung #define nua 
 cout<<PI;
 return 0;
